ffcb56a1daf634c1a37f5f9fbd5b9f08faf7741d
Greg Flores <gflores@xpressdocs.com>
2021-03-09 16:55:00 -0600

    Stuckkk

---
+++ src/components/Board.jsx
@@ -0,0 +1,37 @@
+// import {useState} from 'react'
+import Square from './Square'
+
+const Board = ({squares, onClick}) => {
+    // const [squares, setSquares] = useState(Array(9).fill(null));
+    // const [xIsNext, setXIsNext] = useState(true)
+    
+    const renderSquare = (i) => {
+        return <Square value={squares[i]} onClick={() => onClick(i)}/>
+    }
+
+    
+    return (
+        <div>
+            {/* <div className="status">{status}</div> */}
+            <div className="board-row">
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
+            </div>
+            <div className="board-row">
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
+            </div>
+            <div className="board-row">
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
+            </div>
+        </div>
+    )
+}
+
+
+
+export default Board
